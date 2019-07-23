db9ce215285f6a563d79cbaa96dc8de09d650693
ryanlid <ryanlid@live.com>
2019-07-23 21:44:52 +0800

    implement play sound

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
-    
+    // var xySound = NSURL(fileURLWithPath: Bundle.mainBundle.path(forResource: 'note1', ofType: "wav"))
+    // var player : AVAudioPlayer?
+    var audioPlayer :AVAudioPlayer!
+    var soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        print(sender.tag)
+        playSound(soundFilename: "note" + String(sender.tag))
     }
-    
-  
-
+    func playSound(soundFilename:String) {
+        let soundURL = Bundle.main.url(forResource: soundFilename, withExtension: "wav")
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
+    }
 }
 
